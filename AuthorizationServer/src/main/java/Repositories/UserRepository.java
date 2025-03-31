77e3bb35998f7ef9891da300a3b8f04ff620d794
devart-coder <devart.mail@mail.ru>
2025-03-31 22:41:17 +0300

    * 'AuthorizationServer': Removed duplicating files with 'MusecService' project.Added in 'pom.xml' as depedency 'MusicService' , remake datasource url inside 'application.properties'

--- AuthorizationServer/src/main/java/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import Entities.UserEntity;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-	UserEntity findByUsername(String username);
-}
