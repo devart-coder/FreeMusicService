8608aff841f0268f065d0b1addb98ea7e468d0b1
devart-coder <devart.mail@mail.ru>
2025-04-18 19:26:03 +0300

    On branch test  Changes to be committed: 	modified:   MusicService/src/test/java/MainApplication/MusicServiceApplicationTests.java 	modified:   MusicService/src/test/java/MainApplication/PlayListJpaTesting.java 	deleted:    MusicService/src/test/java/MainApplication/TestConfig.java

--- MusicService/src/test/java/MainApplication/MusicServiceApplicationTests.java
+++ MusicService/src/test/java/MainApplication/MusicServiceApplicationTests.java
@@ -1,25 +1,6 @@
 package MainApplication;
 
-import java.util.UUID;
-
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-
-import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
-
 @SpringBootTest
-=======
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
-
-@SpringBootTest
-@ContextConfiguration(classes = PlayListJpaTesting.class)
->>>>>>> 115b575 ( On branch test)
 class MusicServiceApplicationTests { 	
 }
